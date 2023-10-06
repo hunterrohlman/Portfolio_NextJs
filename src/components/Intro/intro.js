6981d1d00e2d7baeb2ebe87656dfb9dbbf52854a
Hunter Rohlman <hunter.rohlman@gmail.com>
2023-10-06 16:44:03 -0600

    Adjusting quotes

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,23 +1,22 @@
-'use client';
-import React from 'react';
-import './intro.css';
+"use client";
+import React from "react";
+import "./intro.css";
 import bg from "../../assets/linkedinpfpsmall.jpg"
-import btnimg from "../../assets/hireme.png"
-import linkedInImg from '../../assets/linkedin.png'
-import Image from 'next/image';
+import linkedInImg from "../../assets/linkedin.png"
+import Image from "next/image";
 
 const Intro = () => {
   return (
     <section id="intro">
         <div className="introContent"> 
-            <span className='hello'>Hello,</span>
-            <span className='introText'> I'm 
+            <span className="hello">Hello,</span>
+            <span className="introText"> I'm 
                 <span className="introName"> Hunter</span> 
                 <br />Embedded Developer
             </span>
-        <p className='introPara'>I am a skilled embedded developer with a background in 5G NR and space</p>
-          <button className='btn' onClick={()=> window.location.href = "https://www.linkedin.com/in/hunter-rohlman-1611a5159/"}>
-            <Image src={linkedInImg} alt="Hire Me" className='btnimg'/>
+        <p className="introPara">I am a skilled embedded developer with a background in 5G NR and space</p>
+          <button className="btn" onClick={()=> window.location.href = "https://www.linkedin.com/in/hunter-rohlman-1611a5159/"}>
+            <Image src={linkedInImg} alt="Hire Me" className="btnimg"/>
             <p className="btnText">LinkedIn</p>
           </button>
         </div>

c6f89921ce392d87e8a870ba33ac1351fbe34b95
Hunter Rohlman <hunter.rohlman@gmail.com>
2023-10-06 15:35:37 -0600

    Fixing imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Intro from '@/components/Intro/intro';
-import Navbar from '@/components/Navbar/navbar';
-import Skills from '@/components/Skills/skills';
-import Workhistory from '@/components/Workhistory/workhistory';
-import Education from '@/components/Education/education';
-import Contact from '@/components/Contact/contact';
-import Footer from '@/components/Footer/footer';
+import Intro from '../components/Intro/intro';
+import Navbar from '../components/Navbar/navbar';
+import Skills from '../components/Skills/skills';
+import Workhistory from '../components/Workhistory/workhistory';
+import Education from '../components/Education/education';
+import Contact from '../components/Contact/contact';
+import Footer from '../components/Footer/footer';
 import styles from './page.module.css'
 
 export default function Home() {
